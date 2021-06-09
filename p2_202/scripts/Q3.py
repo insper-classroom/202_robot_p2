a0b642977c7056f9cd4fe9a4352221bb4c8b776f
mirwox <fmiranda@gmail.com>
2021-06-09 04:52:44 -0700

    versao feita na revisao de 9/6/2021, falta acertar

--- p2_202/scripts/Q3.py
+++ p2_202/scripts/Q3.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding:utf-8 -*-
 
 from __future__ import print_function, division
@@ -16,11 +16,13 @@ import rospy
 import numpy as np
 
 import cv2
+import cv2.aruco as aruco
 
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import Float64
 
 import math
 
@@ -30,6 +32,75 @@ minv = 0
 maxv = 10
 
 bridge = CvBridge()
+
+### REsposta feita na revisao em 08/6/21
+
+
+def processa_amarelo(bgr): 
+    """ recebe uma imagem bgr e devolve o centro de massa do maior contorno amarelo"""
+    hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+    low = np.array([ 20, 50,50],dtype=np.uint8 )
+    high = np.array([ 38, 255,255],dtype=np.uint8 )
+    mask_yellow = cv2.inRange(hsv, low, high)
+
+    return mask_yellow
+
+## Codigo vindo da aula 03 
+
+def center_of_mass(mask):
+    """ Retorna uma tupla (cx, cy) que desenha o centro do contorno"""
+    M = cv2.moments(mask)
+    # Usando a expressão do centróide definida em: https://en.wikipedia.org/wiki/Image_moment
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+    return [int(cX), int(cY)]
+
+def crosshair(img, point, size, color):
+    """ Desenha um crosshair centrado no point.
+        point deve ser uma tupla (x,y)
+        color é uma tupla R,G,B uint8
+    """
+    x,y = point
+    cv2.line(img,(x - size,y),(x + size,y),color,5)
+    cv2.line(img,(x,y - size),(x, y + size),color,5)
+
+def center_of_mass_region(mask, x1, y1, x2, y2):
+    # Para fins de desenho
+    mask_bgr = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    clipped = mask[y1:y2, x1:x2]
+    c = center_of_mass(clipped)
+    c[0]+=x1
+    c[1]+=y1
+    crosshair(mask_bgr, c, 10, (0,0,255))
+    cv2.rectangle(mask_bgr, (x1, y1), (x2, y2), (255,0,0),2,cv2.LINE_AA)
+    return mask_bgr, c
+
+aruco_dict  = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+
+def detecta_aruco(bgr, dict_aruco):
+
+    gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
+
+    #--- Define the aruco dictionary
+    # parameters  = aruco.DetectorParameters_create()
+    # parameters.minDistanceToBorder = 0
+    # parameters.adaptiveThreshWinSizeMax = 1000
+
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict) #, parameters=parameters)
+
+
+    aruco.drawDetectedMarkers(bgr, corners, ids)
+
+    return corners, ids 
+
+
+centro_imagem = [320, 240]
+centro_massa_amarelo = [320,240]
+
+ids_vistos = []
+
+###
+
 
 
 def scaneou(dado):
@@ -44,10 +115,56 @@ def scaneou(dado):
  
 # A função a seguir é chamada sempre que chega um novo frame
 def roda_todo_frame(imagem):
+    global centro_massa_amarelo
+    global centro_imagem
+
     print("frame")
     try:
         cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
-        cv2.imshow("Camera", cv_image)
+        #cv2.imshow("Camera", cv_image)
+
+        mask_amarela = processa_amarelo(cv_image)
+
+
+        larg = mask_amarela.shape[1]
+        alt  = mask_amarela.shape[0]
+        inicio_y = int(alt/2)
+        fim_y = int((3/4)*alt)
+
+        centro_imagem = [int(larg/2), int(alt/2)]
+
+        mask_bgr, cm = center_of_mass_region(mask_amarela, 0, inicio_y, larg, fim_y)
+        
+        centro_massa_amarelo = cm
+
+        cv2.imshow("Centro de massa", mask_bgr)
+
+        
+        #cv2.imshow("Amarelo", mask_amarela)
+
+
+        corners, ids = detecta_aruco(cv_image, aruco_dict)
+
+        cv2.imshow("Filtro", cv_image)
+
+        ids_vistos.clear()
+
+        tamanho_min = 70
+
+        if len(ids) > 0: 
+
+            for i in range(len(ids)):
+                print('ID: {}'.format(ids[i]))
+
+                ids_vistos.append(ids[i])
+
+
+                #             if len(corners) > 0: 
+                    #
+                    #tamanho = corners[0][0][0] - corners[0][1][0]
+
+                    #if tamanho > tamanho_min: 
+                    
         cv2.waitKey(1)
     except CvBridgeError as e:
         print('ex', e)
@@ -61,5 +178,42 @@ if __name__=="__main__":
     recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
     recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
 
+    ombro = rospy.Publisher("/joint1_position_controller/command", Float64, queue_size=1)
+
+    max_w = 1.4 # rad 
+    v_frente = 0.4 
+    max_delta = 640/2
+
+    zero = Twist(Vector3(0,0,0), Vector3(0,0,0))
+    giro = Twist(Vector3(0,0,0), Vector3(0,0,0.7))
+
+
+    nao_terminou = True 
+
     while not rospy.is_shutdown():
-        rospy.sleep(0.1)
+        ## centro_massa_amarelo 
+        ## centro_imagem
+
+
+
+
+        if (20 not in ids_vistos) and nao_terminou: 
+            delta_x = centro_massa_amarelo[0] - centro_imagem[0]
+
+            rot = -(delta_x * max_w)/max_delta
+
+            vel = Twist(Vector3(v_frente,0,0), Vector3(0,0,rot))
+
+            velocidade_saida.publish(vel)
+        else: 
+            velocidade_saida.publish(zero)
+            ombro.publish(1.5)
+            rospy.sleep(1)
+            velocidade_saida.publish(giro)
+            nao_terminou = False
+
+        if not nao_terminou: 
+            velocidade_saida.publish(giro)            
+
+
+        rospy.sleep(0.05)
